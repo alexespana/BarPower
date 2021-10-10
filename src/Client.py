f318147b7c8ebccbb29f2caafd4b4a09abe207fd
Antobio17 <antoniojr997@gmail.com>
2021-10-10 18:26:03 +0200

    (close #13) Se añade propiedad Region a la entidad Client junto con getter y setter resolviendo el issue #13.

--- src/Client.py
+++ src/Client.py
@@ -1,9 +1,10 @@
 class Client:
 
     # Constructor por parámetros de la entidad.
-    def __init__(self, age):
+    def __init__(self, age, region):
         Client.check_age(age)
         self.age = age
+        self.region = region
 
 
     # Obtiene la propiedad Age de la entidad.
@@ -14,6 +15,14 @@ class Client:
     def set_age(self, age):
         Client.check_age(age)
         self.age = age
+
+    # Obtiene la propiedad Region de la entidad.
+    def get_region(self):
+        return self.region
+
+    # Establece la propiedad Region de la entidad.
+    def set_region(self, region):
+        self.region = region
 
 
     # Método para comprobar que la edad del cliente es correcta.
