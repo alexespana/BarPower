5987f384b6c5197ae212f9a3cbe7d3b273038461
Antobio17 <antoniojr997@gmail.com>
2021-10-10 13:42:00 +0200

    (close #12) Se añade propiedad Age a la entidad Client junto con getter, setter y método para comprobar que la edad establecida es correcta resolviendo el issue #12.

---
+++ src/Client.py
@@ -0,0 +1,23 @@
+class Client:
+
+    # Constructor por parámetros de la entidad.
+    def __init__(self, age):
+        Client.check_age(age)
+        self.age = age
+
+
+    # Obtiene la propiedad Age de la entidad.
+    def get_age(self):
+        return self.age
+
+    # Establece la propiedad Age de la entidad.
+    def set_age(self, age):
+        Client.check_age(age)
+        self.age = age
+
+
+    # Método para comprobar que la edad del cliente es correcta.
+    @staticmethod
+    def check_age(age):
+        if not isinstance(age, int) or age < 3 or age > 100:
+            raise AttributeError("La edad para asignar al cliente no es correcta.")
