d184ec4c4b639041a2a272bead713a5f3501f84b
Antobio17 <antoniojr997@gmail.com>
2021-10-18 19:29:17 +0200

    (close #18) Se añade propiedad data al constructor de la entidad, inicializada como diccionario vacío.

--- bar_power/Visits.py
+++ bar_power/Visits.py
@@ -2,10 +2,18 @@ class Visits:
     """
     Entidad para almacenar la información sobre las visitas al bar de distintos tipos de 
     clientes y las comandas que se hacen en unas franjas horarias.
+
+    Attributes
+    ----------
+    data : dict
+        Datos las visitas al bar de distintos tipos de clientes y las comandas 
+        que se hacen en unas franjas horarias.
     """
 
     def __init__(self):
         """
         Constructor de la entidad
         """
+
+        self.data = {}
 
