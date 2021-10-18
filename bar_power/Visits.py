939ec266f457362a2b9da3ea010646da72cdec3b
Antobio17 <antoniojr997@gmail.com>
2021-10-18 19:26:04 +0200

    (close #17) Se elimina antigua entidad Client y se crear nueva entidad Visits.

---
+++ bar_power/Visits.py
@@ -0,0 +1,11 @@
+class Visits:
+    """
+    Entidad para almacenar la información sobre las visitas al bar de distintos tipos de 
+    clientes y las comandas que se hacen en unas franjas horarias.
+    """
+
+    def __init__(self):
+        """
+        Constructor de la entidad
+        """
+
