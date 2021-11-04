ae71af1e49f97d76117707a2501b028cf28de828
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2021-11-04 18:09:47 +0100

    Enumerado para el tipo de cliente

---
+++ bar_power/ClientType.py
@@ -0,0 +1,12 @@
+import enum
+
+class ClientType(enum.Enum):
+    """
+    Enumerado 
+    ----------
+    Contiene los tipos de cliente disponibles
+    """
+    ninio = 1
+    joven = 2
+    adulto = 3
+    anciano = 4
