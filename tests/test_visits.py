708001d03a3fa5016a14810b7dccbcef5b14cb7b
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2021-11-04 18:38:28 +0100

    Nuevos tests añadidos

--- tests/test_visits.py
+++ tests/test_visits.py
@@ -1,26 +1,35 @@
 import pytest   # Biblioteca de aserciones
-import bar_power.Visits
+from bar_power.Visits import *
+from bar_power.ClientType import *
+
+HOUR = 19
+PRODUCT = 'paella'
+CLIENT_TYPE = ClientType.joven
+HOUR_WITH_FORMAT = Visits.get_hour_from_time(HOUR)  # 19:00
 
 @pytest.fixture
 def visits():
-    visits = bar_power.Visits.Visits()
+    visits = Visits()
     return visits
 
-def test_has_new_entry_when_added_visit(visits):
-    visits.add_visit('joven', 10)
-    hour = bar_power.Visits.Visits.get_hour_from_time(10)
-    assert visits.data[hour]['clients_type']['joven'] == 1
+def test_empty_visits_when_creation(visits):
+    assert len(visits.v_visits) == 0
 
-def test_has_new_entry_when_added_product(visits):
-    visits.add_product_consumed('paella', 14)
-    hour = bar_power.Visits.Visits.get_hour_from_time(14)
-    assert visits.data[hour]['products_consumed']['paella'] == 1
+def test_empty_orders_when_creation(visits):
+    assert len(visits.v_orders) == 0
 
-def test_new_value_object_in_v_visits(visits):
-    visits.add_visit('anciano', 19)
+def test_update_data_when_added_visit(visits):
+    visits.add_visit(CLIENT_TYPE, HOUR)
+    assert visits.data[HOUR_WITH_FORMAT]['clients_type'][CLIENT_TYPE] == 1
+
+def test_update_data_when_added_product(visits):
+    visits.add_product_consumed(PRODUCT, HOUR)
+    assert visits.data[HOUR_WITH_FORMAT]['products_consumed'][PRODUCT] == 1
+
+def test_new_visit_added(visits):
+    visits.add_visit(CLIENT_TYPE, HOUR)
     assert len(visits.v_visits) == 1
 
-def test_new_value_object_in_v_orders(visits):
-    visits.add_product_consumed('Carne en salsa', 15)
+def test_new_order_added(visits):
+    visits.add_product_consumed(PRODUCT, HOUR)
     assert len(visits.v_orders) == 1
-    
