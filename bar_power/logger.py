a17bb8c3fbadef990fc68490ab9fc803479e0d6d
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2021-12-17 13:43:06 +0100

    Incluye captura de excepcion #46

--- bar_power/logger.py
+++ bar_power/logger.py
@@ -1,11 +1,14 @@
 import logging, os
 from bar_power.config import Config
 
-configutation = Config()
-LOG_DIRECTORY = configutation.get_log_directory()
-LOG_FILE      = configutation.get_log_file()
+configuration = Config()
+LOG_DIRECTORY = configuration.get_log_directory()
+LOG_FILE      = configuration.get_log_file()
 
 if not os.path.exists(LOG_DIRECTORY):
-    os.makedirs(LOG_DIRECTORY, exist_ok=True)
+    try:
+        os.makedirs(LOG_DIRECTORY, exist_ok=True)
+    except OSError:
+        raise OSError("Error al intentar crear el directorio ")
 
 logging.basicConfig(filename=LOG_DIRECTORY + LOG_FILE, level=logging.DEBUG, format='%(asctime)s <=> %(levelname)s <=> %(pathname)s <=> %(message)s' )
